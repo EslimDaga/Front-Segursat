56550f565db5665e305816b48405aad67435667b
Eslim Daga <edaga@segursat.com>
2020-12-29 15:23:45 -0500

    Add class map

--- static/assets/js/maps/MapView.js
+++ static/assets/js/maps/MapView.js
@@ -1,117 +1,3 @@
-/* 
-let units;
-let markers = [];
-
-const getUnits = async () => {
-  const url = `/web/api/units/get-units/`
-  const response = await fetch(url);
-  const units = await response.json();
-  return units;
-}
-const getUnit = async (name) => {
-  const url = `/web/api/units/get-unit/${name}/`;
-  const response = await fetch(url);
-  const unit = await response.json();
-  return unit;
-}
-const renderUnitsInSidebar = async() => {
-  units = await getUnits();
-  let unitList = "";
-  for (let i = 0; i < units.length; i++) {
-    const unit =
-    `
-      <div onclick="unitSelect('${units[i].name}')" class="items">
-        <div class="user-name">
-          <p class="">${units[i].name}</p>
-        </div>
-        <div class="user-status">
-          <span class="badge badge-primary">
-            <div class="details">
-              <span data-device="speed">${units[i].device.last_speed} kph</span>
-              <span data-device="detect_engine" class="on"><i class="icon detect_engine"></i></span>
-              <span data-device="status" style="background-color: green" title="Online">online</span>
-            </div>
-          </span>
-        </div>
-      </div>
-    `;
-    unitList += unit;
-  }
-  document.getElementById("unit-list").innerHTML = unitList;
-}
-const unitSelect = async (unitName) => {
-  const unit = await getUnit(unitName);
-  document.getElementById("unitNameSelect").innerHTML = unitName;
-  map.panTo([unit.device.last_latitude,unit.device.last_longitude]);
-}
-const renderMap = async () => {
-  map = L.map('map',{
-    center: [-12.004502, -77.094823],
-    zoom: 12,
-  });
-  const openStreetMap = L.tileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {
-      attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
-      maxZoom: 19,
-    }
-  );
-  const googleStreetsMap = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
-    attribution: '&copy; <a href="https://maps.google.com/">Google Maps</a>',
-    maxZoom: 20,
-    subdomains:['mt0','mt1','mt2','mt3']
-  });
-  const googleHybridMap = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
-    attribution: '&copy; <a href="https://maps.google.com/">Google Maps</a>',
-    maxZoom: 20,
-    subdomains:['mt0','mt1','mt2','mt3']
-  });
-  const baseLayers = {
-    'Open Street Maps': openStreetMap,
-    'Google Street Maps': googleStreetsMap,
-    'Google Hybrid Maps': googleHybridMap
-  };
-  let overlays = {};
-  L.control.layers(baseLayers,overlays).addTo(map);
-  baseLayers['Google Street Maps'].addTo(map);
-
-  // getUnits
-  const units = await getUnits();
-
-  for (let i = 0; i < units.length; i++) {
-    const icon = L.divIcon({
-      iconSize:null,
-      html:`
-      <div class="map-label">
-        <div class="map-label-content">
-          <img src="/static/assets/img/markers/vehicle.png" width="26" height="48"/>
-        </div>
-      </div>
-      `
-    });
-    markers.push(
-      new L.marker(
-        [units[i].device.last_latitude,units[i].device.last_longitude],
-        {
-          icon: icon,
-          title: units[i].name,
-          rotationAngle: 0
-        }
-      ).bindTooltip(`${units[i].name}`, {
-        permanent: true,
-        direction : "top",
-        offset: L.point({x: 0, y: -30})
-      })
-      .addTo(map)
-    );
-  }
-
-}
-
-renderUnitsInSidebar();
-renderMap(); */
-
-
 class MapView {
 
   constructor() {
@@ -214,6 +100,7 @@ class MapView {
       );
     }
   }
+
   searchUnitMarker = (unitName) => {
     let index = -1
     for (let i=0;i<this.markers.length;i++) {
@@ -224,19 +111,41 @@ class MapView {
     }
     return index;
   }
-  unitSelect = (unitName) => {
+
+  unitSelect = async (unitName) => {
     const index = this.searchUnitMarker(unitName);
     const lat = this.markers[index].getLatLng().lat
     const lng = this.markers[index].getLatLng().lng
     const streetViewURL = `http://maps.google.com/maps?q=&amp;layer=c&amp;cbll=${lat},${lng}&amp;cbp=11,96,0,0,0`;
     this.map.panTo([lat,lng]);
+    //almacenar en el local storage la unidad seleccionada
+    localStorage.setItem("selectedUnit", unitName);
+    //llamar a get unit
+    const unit = await this.getUnit(unitName);
+    console.log(unit);
+    //
     document.getElementById("unitNameSelect").innerHTML = unitName;
     document.getElementById("streetView").innerHTML = `<a href='${streetViewURL}' target="_blank"><img alt="Street view" src="https://iadpi.com.ar/wp-content/uploads/2019/05/google-street-696x355.jpg"></a>`;
+    document.getElementById("speed").innerHTML = `${unit.device.last_speed} km/h`;
+    document.getElementById("rssi").innerHTML = `${unit.device.last_attributes.rssi}`;
+    document.getElementById("battery").innerHTML = `${unit.device.last_attributes.battery}`;
+    document.getElementById("address_link").innerHTML = `<a href='https://www.google.com/maps?q=${lat},${lng}&t=m&hl=en' target="_blank" class="btn btn-xs btn-default">
+    <i class="fas fa-eye"></i>
+  </a>`
+    document.getElementById("address").innerHTML = `${unit.device.last_address}`;
   }
+
   updateMarkerPosition = (unitName,latLng,angle) => {
     const index = this.searchUnitMarker(unitName);
     this.markers[index].setLatLng(latLng);
     this.markers[index].setRotationAngle(angle);
+  }
+
+  updateBottomBar = (data) => {
+    const selectedUnit = localStorage.getItem("selectedUnit")
+    if (selectedUnit == data['unit_name']) {
+      console.log(`Actualizar informacion del bottom bar ${unitName}`)
+    }
   }
 
   run = async () => {
