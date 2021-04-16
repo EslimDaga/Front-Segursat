294425f0cd9cca98fc5b1bb13b2732f22d12f834
Eslim Daga <esliim9811@gmail.com>
2021-04-16 12:38:29 -0500

    Add content script websocket

--- static/assets/js/pages/common/websocket.js
+++ static/assets/js/pages/common/websocket.js
@@ -1,40 +1,38 @@
-  const mapView = new MapView();
-  mapView.run();
-  let ws;
-    const getWSURI = () => {
-    let domain = window.location.hostname;
-      let port = 8000;
-      if (location.port == ""){
-    port = 80;
-      }
-      wsURI = `ws://${domain}:${port}/ws/locations/{{ user.profile.account }}/`;
-      return wsURI;
+class MyWebsocket{
+
+  constructor(account) {
+    this.ws;
+    this.account = account
+  }
+  getWSURI = () => {
+    const domain = window.location.hostname;
+    let port = 8000;
+    if (location.port == ""){
+      port = 80;
     }
-    // websocket
-    const connect = () => {
-    wsURI = getWSURI();
-      ws = new WebSocket(wsURI);
-      ws.onopen = function() {
-    console.log('Socket is connected!');
-      };
-      ws.onmessage = function(e) {
-    newLocation = JSON.parse(e.data).message;
-        /* console.log(newLocation); */
-        mapView.updateMarkerPosition(
-          newLocation['unit_name'],
-          [
-            newLocation['latitude'],
-            newLocation['longitude']
-          ],
-          newLocation['angle']
-        );
-      };
-      ws.onclose = function(e) {
-    console.log('Socket is closed. Reconnect will be attempted in 5 second.', e.reason);
-      };
-      ws.onerror = function(err) {
-    console.error('Socket encountered error: ', err.message, 'Closing socket');
-        ws.close();
-      };
-    }
-    connect();
+    const wsURI = `ws://${domain}:${port}/ws/locations/${this.account}/`;
+    return wsURI;
+  }
+  // websocket
+  run = () => {
+    const wsURI = this.getWSURI();
+    this.ws = new WebSocket(wsURI);
+    this.ws.onopen = function() {
+      console.log('Socket is connected!');
+    };
+    this.ws.onmessage = function(e) {
+      console.log("Mensaje, mensajeee");
+    };
+    this.ws.onclose = function(e) {
+      console.log('Socket is closed. Reconnect will be attempted in 5 second.', e.reason);
+      setTimeout(function() {
+        this.run();
+      }, 5000);
+    };
+    this.ws.onerror = function(err) {
+      console.error('Socket encountered error: ', err.message, 'Closing socket');
+      this.ws.close();
+    };
+  }
+
+}
